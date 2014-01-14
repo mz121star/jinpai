221fe8bf2776cec5f02239c09993eef8034937e8
mz121star <mz121star@gmail.com>
2014-01-14 16:56:46 +0800

    miaozhuang

--- js/index.js
+++ js/index.js
@@ -1,20 +1,30 @@
 $(function () {
     var viewPlaceHolder = $("#view");
-    var SetPage=function(url,data){
+    var SetPage = function (url, data) {
         mHelper.render(url, data, function (html) {
             viewPlaceHolder.html(html);
+            var duoshuoQuery = {short_name: "dalianjinpai"};
+
+            var ds = document.createElement('script');
+            ds.type = 'text/javascript';
+            ds.async = true;
+            ds.src = 'http://static.duoshuo.com/embed.js';
+            ds.charset = 'UTF-8';
+            (document.getElementsByTagName('head')[0]
+                || document.getElementsByTagName('body')[0]).appendChild(ds);
+
         });
     }
     hashRouter({
-         '':function(){
-             SetPage("index.tmpl", {});
-         },
+        '': function () {
+            SetPage("index.tmpl", {});
+        },
         'companyintro': function () {
 
-            SetPage("content-page.tmpl", "company-intro.json" );
+            SetPage("content-page.tmpl", "company-intro.json");
         },
-        'management-consulting':function(){
-            SetPage("content-page.tmpl", "management-consulting.json" );
+        'management-consulting': function () {
+            SetPage("content-page.tmpl", "management-consulting.json");
         }
     })
 
